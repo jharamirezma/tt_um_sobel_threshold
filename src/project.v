62424b7157efcd3050003c41912abb77016d7fed
jharamirezma <jharamirezma@unal.edu.co>
2026-05-01 14:04:58 -0500

    Starting...

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_un_sobel_threshold (
+module tt_um_sobel_threshold (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
